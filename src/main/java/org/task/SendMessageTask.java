867b60415aa56ef01b883316d7c666ed810f1682
E200394 <s.vandesande@student.helmo.be>
2023-02-08 23:29:51 +0100

    Ajout des taches MsgsMessageTask et SendMessageTask

---
+++ src/main/java/org/task/SendMessageTask.java
@@ -0,0 +1,13 @@
+package org.task;
+
+public class SendMessageTask implements Task{
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
--- src/main/java/org/task/Task.java
+++ src/main/java/org/task/Task.java
@@ -6,4 +6,3 @@ public interface Task {
 
     void execute();
 }
-
