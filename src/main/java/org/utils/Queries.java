a01b816fdb921515b81f1ec9db7f2ab93f71d7e1
Quentin <q.barthelemy@student.helmo.be>
2023-02-10 13:42:29 +0100

    Added NetChooser Update Regexes & Queries

--- src/main/java/org/utils/Queries.java
+++ src/main/java/org/utils/Queries.java
@@ -5,10 +5,10 @@ package org.utils;
  */
 public class Queries {
 
-    public static final String HELLO = "HELLO %s %s \r\n";
-    public static final String PARAM = "PARAM %i %s \r\n";
-    public static final String MSGS = "MSGS %s %s \r\n";
-    public static final String OK = "+OK[ %s] \r\n";
-    public static final String ERREUR = "-ERR[ %s] \r\n";
+    public static final String HELLO = "HELLO %s %s\r\n";
+    public static final String PARAM = "PARAM %i %s\r\n";
+    public static final String MSGS = "MSGS %s %s\r\n";
+    public static final String OK = "+OK[ %s]\r\n";
+    public static final String ERREUR = "-ERR[ %s]\r\n";
 
 }
--- src/main/java/org/utils/Regexes.java
+++ src/main/java/org/utils/Regexes.java
@@ -1,5 +1,7 @@
 package org.utils;
 
+import java.util.regex.Pattern;
+
 public class Regexes {
 
     public static final String DOMAIN = "^[a-zA-Z\\d.]{5,200}$";
@@ -8,7 +10,7 @@ public class Regexes {
 
     public static final String ROUND_OR_SALT_SIZE = "^\\d{2}$";
 
-    public static final String BCRYPT_HASH = "^[2][b][\\$]\\d{2}[a-zA-Z\\d\\x21-\\x2F\\x3A\\x-40\\x5B-\\x60]{1,70}$";
+    public static final String BCRYPT_HASH = "^[2][b][$]\\d{2}[$][a-zA-Z\\d\\x21-\\x2F\\x3A-\\x40\\x5B-\\x60]{1,70}$";
 
     public static final String SHA3_HEX ="^[a-zA-Z\\d]{30,200}$";
 
@@ -29,20 +31,28 @@ public class Regexes {
     public static final String ID_DOMAIN = "^[\\d]{1,5}[@][a-zA-Z\\d.]{5,200}$";
 
 
-    //Received commands from clients
+    //Received commands matchers pattern
 
-    public static final String CONNECT = "^[C][O][N][N][E][C][T][\\x20][a-zA-Z\\d]{5,20}[\\xD][\\xA]$";
+    public static final Pattern CONNECT = Pattern.compile("^[C][O][N][N][E][C][T][\\x20]([a-zA-Z\\d]{5,20})[\\x0D][\\x0A]$");
 
-    public static final String REGISTER = "^[R][E][G][I][S][T][E][R][\\x20][a-zA-Z\\d]{5,20}[\\x20]\\d{2}[\\x20][2][b][\\$]\\d{2}[a-zA-Z\\d\\x21-\\x2F\\x3A\\x-40\\x5B-\\x60]{1,70}[\\xD][\\xA]$";
+    public static final Pattern REGISTER = Pattern.compile("^[R][E][G][I][S][T][E][R][\\x20]([a-zA-Z\\d]{5,20})[\\x20](\\d{2})[\\x20][2][b][$](\\d{2})[$]([a-zA-Z\\d\\x21-\\x2F\\x3A-\\x40\\x5B-\\x60]{1,70})[\\x0D][\\x0A]$");
 
-    public static final String DISCONNECT = "^[D][I][S][C][O][N][N][E][C][T][\\xD][\\xA]$";
+    public static final Pattern DISCONNECT = Pattern.compile("^[D][I][S][C][O][N][N][E][C][T][\\x0D][\\x0A]$");
 
-    public static final String CONFIRM = "^[C][O][N][F][I][R][M][\\x20][a-zA-Z\\d]{30,200}[\\xD][\\xA]$";
+    public static final Pattern CONFIRM = Pattern.compile("^[C][O][N][F][I][R][M][\\x20]([a-zA-Z\\d]{30,200})[\\x0D][\\x0A]$");
 
-    public static final String MSG = "^[M][S][G][\\x1F][\\x20-\\xFF]{1,200}[\\xD][\\xA]$";
+    public static final Pattern MSG = Pattern.compile("^[M][S][G][\\x1F]([\\x20-\\xFF]{1,200})[\\x0D][\\x0A]$");
 
-    public static final String FOLLOW_DOMAIN = "^[F][O][L][L][O][W][\\x20][a-zA-Z\\d]{5,20}[@][a-zA-Z\\d.]{5,200}[\\xD][\\xA]$";
+    public static final Pattern FOLLOW_DOMAIN = Pattern.compile("^[F][O][L][L][O][W][\\x20]([a-zA-Z\\d]{5,20}[@][a-zA-Z\\d.]{5,200})[\\x0D][\\x0A]$");
 
-    public static final String FOLLOW_TAG = "^[F][O][L][L][O][W][\\x20][#][a-zA-Z\\d]{5,20}[@][a-zA-Z\\d.]{5,200}[\\xD][\\xA]$";
+    public static final Pattern FOLLOW_TAG = Pattern.compile("^[F][O][L][L][O][W][\\x20]([#][a-zA-Z\\d]{5,20}[@][a-zA-Z\\d.]{5,200})[\\x0D][\\x0A]$");
+
+    //Multicast
+    public static final Pattern ECHO = Pattern.compile("^[E][C][H][O][\\x20](\\d{1,5})[\\x20]([a-zA-Z\\d.]{5,200})[\\x0D][\\x0A]");
+
+    //Unicast
+    public static final Pattern SEND_NAME = Pattern.compile("[S][E][N][D][\\x20]([\\d]{1,5}[@][a-zA-Z\\d.]{5,200})[\\x20]([a-zA-Z\\d]{5,20}[@][a-zA-Z\\d.]{5,200})[\\x20]([a-zA-Z\\d]{5,20}[@][a-zA-Z\\d.]{5,200})[\\x20]([\\x20-\\xFF]{1,500})[\\x0D][\\x0A]");
+
+    public static final Pattern SEND_TAG = Pattern.compile("[S][E][N][D][\\x20]([\\d]{1,5}[@][a-zA-Z\\d.]{5,200})[\\x20]([a-zA-Z\\d]{5,20}[@][a-zA-Z\\d.]{5,200})[\\x20]([#][a-zA-Z\\d]{5,20}[@][a-zA-Z\\d.]{5,200})[\\x20]([\\x20-\\xFF]{1,500})[\\x0D][\\x0A]");
 
 }
