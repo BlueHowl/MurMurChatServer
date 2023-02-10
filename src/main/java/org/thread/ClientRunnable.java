79f1d123e28f9f57eb63d1ede77350e7402e3e32
e200971 <a.vandenbroeck@student.helmo.be>
2023-02-10 16:03:30 +0100

    HELLO & REGISTER

--- src/main/java/org/thread/ClientRunnable.java
+++ src/main/java/org/thread/ClientRunnable.java
@@ -7,6 +7,7 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class ClientRunnable implements Runnable {
+    private static final int SIZE = 22;
     private final Socket client;
     private BufferedReader in;
     private PrintWriter out;
@@ -19,7 +20,8 @@ public class ClientRunnable implements Runnable {
         try {
             in = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
             out = new PrintWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8), true);
-            sendMessage("HELLO localhost aaaaaaaaaaaaaaaaaaaaaa\r\n");
+            String key = randomString(SIZE);
+            sendMessage("HELLO localhost " + key + "\r\n");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -37,8 +39,16 @@ public class ClientRunnable implements Runnable {
         }
 
     }
-
     private void sendMessage(String message) {
         out.println(message);
+    }
+    private String randomString(int size) {
+        String possibilites = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!%^.?@#&/*";
+        StringBuilder random = new StringBuilder();
+        for (int i = 0 ; i < size; i++) {
+            int character = (int) (Math.random() * (size));
+            random.append(possibilites.charAt(character));
+        }
+        return random.toString();
     }
 }
