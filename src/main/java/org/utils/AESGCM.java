2c130e540b392021be21b08946cb26ce6b665d57
Quentin <q.barthelemy@student.helmo.be>
2023-03-09 18:57:01 +0100

    removed aes tag

--- src/main/java/org/utils/AESGCM.java
+++ src/main/java/org/utils/AESGCM.java
@@ -18,27 +18,6 @@ public class AESGCM {
         this.aeskey = aeskey;
     }
 
-    /*public String decrypt(String cipherText, String ivNonce) {
-        try {
-            return encryptDecrypt(cipherText, Cipher.DECRYPT_MODE, ivNonce);
-        } catch (Exception e) {
-            System.out.println("Failed to decrypt text");
-        }
-
-        return null;
-    }*/
-
-    /*
-    public String encrypt(String plaintext) {
-        try {
-            return encryptDecrypt(plaintext, Cipher.ENCRYPT_MODE, null);
-        } catch (Exception e) {
-            System.out.println("Failed to encrypt text " + e.getMessage());
-        }
-
-        return null;
-    }*/
-
     public String encrypt(String plaintext) {
             try {
             // Generate a secure random nonce
@@ -58,11 +37,9 @@ public class AESGCM {
             byte[] ciphertext = cipher.doFinal(plaintext.getBytes(StandardCharsets.UTF_8));
 
             // Concatenate the nonce, ciphertext, and tag into a single byte array
-            byte[] encrypted = new byte[GCM_IV_LENGTH + ciphertext.length + GCM_TAG_LENGTH];
+            byte[] encrypted = new byte[GCM_IV_LENGTH + ciphertext.length];
             System.arraycopy(nonce, 0, encrypted, 0, GCM_IV_LENGTH);
             System.arraycopy(ciphertext, 0, encrypted, GCM_IV_LENGTH, ciphertext.length);
-            byte[] dummytag = new byte[16];
-            System.arraycopy(dummytag, 0, encrypted, GCM_IV_LENGTH + ciphertext.length, GCM_TAG_LENGTH);
 
             // Encode the result as a Base64 string
             return Base64.getEncoder().encodeToString(encrypted);
@@ -79,13 +56,11 @@ public class AESGCM {
 
             // Extract the nonce, ciphertext, and tag from the encrypted bytes
             byte[] nonce = new byte[GCM_IV_LENGTH];
-            byte[] ciphertext = new byte[encryptedBytes.length - GCM_IV_LENGTH - GCM_TAG_LENGTH];
-            byte[] tag = new byte[GCM_TAG_LENGTH];
+            byte[] ciphertext = new byte[encryptedBytes.length - GCM_IV_LENGTH];
 
             //copie des informations dans les bonnes variables
             System.arraycopy(encryptedBytes, 0, nonce, 0, GCM_IV_LENGTH);
             System.arraycopy(encryptedBytes, GCM_IV_LENGTH, ciphertext, 0, ciphertext.length);
-            System.arraycopy(encryptedBytes, GCM_IV_LENGTH + ciphertext.length, tag, 0, GCM_TAG_LENGTH);
 
             SecretKey key = new SecretKeySpec(aeskey, "AES");
 
@@ -105,50 +80,4 @@ public class AESGCM {
         }
     }
 
-    /*private String encryptDecrypt(String cipherText, int cipherMode, String ivNonce) throws Exception {
-        //TODO faire commentaire
-        //
-        byte[] IV = new byte[GCM_IV_LENGTH]; //todo genere aléatoirement ?
-
-        // Get Cipher Instance
-        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-
-        // Create SecretKeySpec
-        SecretKeySpec keySpec = new SecretKeySpec(aeskey.getEncoded(), "AES");
-
-        if(ivNonce != null) {
-            IV = ivNonce.getBytes();
-        } else {
-            random.nextBytes(IV);
-        }
-
-        // Create GCMParameterSpec
-        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH * 8, IV);
-
-        // Initialize Cipher
-        cipher.init(cipherMode, keySpec, gcmParameterSpec);
-
-        byte[] cypherBytes = new byte[0];
-        if(cipherMode == 1) {
-            cypherBytes = Base64.getEncoder().encode(cipherText.getBytes("UTF-8"));
-        } else if(cipherMode == 2) {
-            cypherBytes = Base64.getDecoder().decode(cipherText.getBytes("UTF-8"));
-        }
-        System.out.println("ciphertext : " + cipherText);
-        System.out.println("nonceIV : " + Arrays.toString(IV));
-        // Perform Decryption
-        byte[] finalText = cipher.doFinal(cypherBytes);
-
-        System.out.println("out : " + String.format("%S %S", new String(IV, StandardCharsets.UTF_8), new String(finalText, StandardCharsets.UTF_8)));
-
-        System.out.println("ciphermode : " + cipherMode);
-        System.out.println("base64 decoded result : " + Arrays.toString(Base64.getDecoder().decode(finalText)));
-
-        //si encrypt alors on retourne le vecteur IV espace la chaine cryptée
-        if(cipherMode == 1) {
-            return String.format("%S %S", new String(IV, StandardCharsets.UTF_8), new String(finalText, StandardCharsets.UTF_8));
-        }
-
-        return new String(finalText);
-    }*/
 }
